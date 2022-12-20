53465315d410c7ab6583fdb739e7293e3082c0be
Luciano Bitencourt <lucbite@gmail.com>
2022-12-20 08:13:28 -0300

    update
    update

--- js/script.js
+++ js/script.js
@@ -8,4 +8,25 @@ Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aperiam, quod voluptas
 
 // Comentario de uma unica linha
 
-var nomePessoa 
+// Declarando Variaveis
+
+// string 
+var nome = prompt("Digite seu nome",'seu nome');
+
+// Number
+var idade = Number(prompt("Sua idade","Digite aqui"));
+
+var altura = Number(prompt("Sua idade","Digite aqui"));
+
+// boolean
+var texte_1 = true;
+var texte_2 = false;
+
+console.log(typeof idade);
+
+
+// alert("O sr." + nome + " tem "+idade+ " de idade " + " e " + altura + " de altura")
+
+console.log("O sr." + nome + " tem "+idade+ " de idade " + " e " + altura + " de altura");
+
+document.write("<h3>O sr° " + nome + " tem "+idade+ " de idade " + " e " + altura + " de altura.</h3> ")
