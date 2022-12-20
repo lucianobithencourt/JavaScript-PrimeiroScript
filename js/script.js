3465ba1d33c2ca04e9272963770d12911e2330d9
Luciano Bitencourt <lucbite@gmail.com>
2022-12-20 07:05:38 -0300

    .
    .

---
+++ js/script.js
@@ -0,0 +1,11 @@
+
+// alert("Primeiro Script")
+
+/* ----Comentário de Varias linhas ----
+ 
+Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aperiam, quod voluptas. Fugit delectus, rem totam voluptatum facilis exercitationem recusandae optio sunt ex cum quisquam similique alias necessitatibus reiciendis tempora quas? 
+ */
+
+// Comentario de uma unica linha
+
+var nomePessoa 
