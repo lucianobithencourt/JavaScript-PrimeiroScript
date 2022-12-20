d960144e7c37ee7e64e133f191930a2fc190deda
Luciano Bitencourt <lucbite@gmail.com>
2022-12-20 08:47:07 -0300

    up
    up

--- js/script.js
+++ js/script.js
@@ -11,17 +11,18 @@ Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aperiam, quod voluptas
 // Declarando Variaveis
 
 // string 
-var nome = prompt("Digite seu nome",'seu nome');
+// var nome = prompt("Digite seu nome",'seu nome');
 
 // Number
-var idade = Number(prompt("Sua idade","Digite aqui"));
+// var idade = Number(prompt("Sua idade","Digite aqui"));
 
-var altura = Number(prompt("Sua idade","Digite aqui"));
+// var altura = Number(prompt("Sua idade","Digite aqui"));
 
 // boolean
-var texte_1 = true;
-var texte_2 = false;
+// var texte_1 = true;
+// var texte_2 = false;
 
+/** Exibir Script no cosole e tela 
 console.log(typeof idade);
 
 
@@ -29,4 +30,29 @@ console.log(typeof idade);
 
 console.log("O sr." + nome + " tem "+idade+ " de idade " + " e " + altura + " de altura");
 
-document.write("<h3>O sr° " + nome + " tem "+idade+ " de idade " + " e " + altura + " de altura.</h3> ")
+document.write("<h3>O sr° " + nome + " tem "+idade+ " de idade " + " e " + altura + " de altura.</h3> ")*/ 
+
+// TROCAR VALORES DE UMA VARIAVEL
+
+var A = 10;
+var B = 20;
+var C = null;
+
+console.log(A);
+console.log(B);
+console.log(C);
+// ---------------------------
+
+var C = A;
+var A = B;
+var B = C;
+
+
+console.log(A);
+console.log(B);
+console.log(C);
+
+
+
+
+
